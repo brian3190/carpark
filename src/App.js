d2dbbae002541be17932e9a45aa52a7bf0c7a3ab
Brian Yap <brian3190@gmail.com>
2023-02-02 04:38:01 +0800

    api working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
+import axios from 'axios';
 //import { Route } from 'react-router';
 //import { Home } from './components/Home';
 //import { Layout } from './components/Layout';
@@ -12,44 +13,66 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.populateCarparkData();
+    setTimeout(() => {
+      axios.get('https://api.data.gov.sg/v1/transport/carpark-availability')
+        .then(resp => {
+          console.log(resp);
+        this.setState({
+          data: resp.data.items[0].carpark_data,
+          loading: false
+        })
+      })
+      .catch(console.error);
+    }, 1000);
   }
 
   static renderCarpark(data) {
     return (
       <div>
-          <div >
-            <h2>SMALL</h2>
-            <h2>HIGHEST</h2>
-            <p>
-            {[data].map(c => (
-              <h3>
-                {c.items.carpark_data.carpark_number}
-              </h3>
-            ))}
-            </p>
+        <div >
+          <h2>SMALL</h2>
+          <h4>HIGHEST</h4>
+          <div>
+            {data.length}
           </div>
-          <div >
-            <h2>MEDIUM</h2>
-            <h2>HIGHEST</h2>
-            <p>
-
-            </p>
+          <h4>LOWEST</h4>
+          <div>
+            {data[0].carpark_number}
+          </div> 
+        </div>
+        <div >
+          <h2>MEDIUM</h2>
+          <h4>HIGHEST</h4>
+          <div>
+            {data.length}
           </div>
-          <div >
-            <h2>BIG</h2>
-            <h2>HIGHEST</h2>
-            <p>
-
-            </p>
+          <h4>LOWEST</h4>
+          <div>
+            {data.length}
           </div>
-          <div >
-            <h2>LARGE</h2>
-            <h2>HIGHEST</h2>
-            <p>
-
-            </p>
+        </div>
+        <div>
+          <h2>BIG</h2>
+          <h4>HIGHEST</h4>
+          <div>
+            {data.length}
           </div>
+          <h4>LOWEST</h4>
+          <div>
+            {data.length}
+          </div>
+        </div>
+        <div>
+          <h2>LARGE</h2>
+          <h4>HIGHEST</h4>
+          <div>
+            {data.length}
+          </div>
+          <h4>LOWEST</h4>
+          <div>
+            {data.length}
+          </div>
+        </div>
       </div>
     )
   }
@@ -57,14 +80,14 @@ export default class App extends Component {
   render() {
     let contents = this.state.loading
       ? <p><em>Loading...</em><br/><img src={logo} className="App-logo" alt="logo" /></p>
-      : App.renderCarpark(this.state.carpark_data);
+      : App.renderCarpark(this.state.data);
     
     return (
       <div className="App">
         <header className="App-header">
-          <p>
+          <div>
             Sample WEBAPP for Carpark
-          </p>
+          </div>
         </header>
         {contents}
       </div>
@@ -81,8 +104,6 @@ export default class App extends Component {
   }
   
   async populateCarparkData() {
-    const response = await fetch('https://api.data.gov.sg/v1/transport/carpark-availability');
-    const data = await response.json();
-    this.setState({ carpark_data: data, loading: false });
+    
   }
 }
