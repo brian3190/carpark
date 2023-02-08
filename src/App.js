f40975f628353c2bc5ffd1ed55ea140f8a79aba1
Brian Yap <brian3190@gmail.com>
2023-02-08 23:34:49 +0800

    include ejs view template for node to render react html

--- src/App.js
+++ src/App.js
@@ -11,26 +11,28 @@ export default class App extends Component {
   //small_carpark, medium_carpark etc = array of carpark obtained from filtered data
   //s_highest, s_lowest, m_highest, m_lowest etc = highest available carpark and lowest available carpark
   //lowest, highest = temp value store for lowest and highest value for generic comparison function findHighest and findLowest
-  constructor(props) {
-    //let data;
-    super(props);
-    //this.processData(data) = this.processData(data).bind(this);
-    this.state = {
-      data: [], loading: true, small_high_avail: 50, small_low_avail: 0, med_high_avail: 0, med_low_avail: 0, big_high_avail: 0, big_low: 0, large_high: 0, large_low: 0,
-      small_carpark: [], medium_carpark: [], big_carpark: [], large_carpark: [], s_highest: [], s_lowest: [], m_highest: [], m_lowest: [], b_highest: [], b_lowest: []
-    }
-    this.small_carpark = [];
-    this.medium_carpark = [];
-    this.big_carpark = [];
-    this.large_carpark = [];
-    this.b_highest = {};
-    this.b_lowest = {};
-    this.m_highest = {};
-    this.m_lowest = {};
-    this.highest = 0;
-    this.lowest = 0;
-    
-  };
+  state = {
+      data: [],
+      loading: true,
+      small_high_avail: 50,
+      small_low_avail: 20,
+      med_high_avail: 0,
+      med_low_avail: 0,
+      big_high_avail: 0,
+      big_low: 0,
+      large_high: 0,
+      large_low: 0,
+      small_carpark: [],
+      medium_carpark: [],
+      big_carpark: [],
+      large_carpark: [],
+      s_highest: [],
+      s_lowest: [],
+      m_highest: [],
+      m_lowest: [],
+      b_highest: [],
+      b_lowest: []
+    }    
 
   //Array initialization
   //let small_carpark = [];
@@ -97,52 +99,51 @@ export default class App extends Component {
   }
 
   // Render Carpark component into App
-  static renderCarpark(data, small_high, small_low, med_high, med_low, big_high, big_low, large_high, large_low,
-      small_carpark, medium_carpark, big_carpark, large_carpark, s_highest, s_lowest, m_highest, m_lowest, b_highest, b_lowest) {
+  static renderCarpark(state) {
     return (
       <div>
         <div >
           <h2>SMALL</h2>
-          <h4>HIGHEST ({this.small_high_avail} lots available)</h4>
+          <h4>HIGHEST ({state.small_high_avail} lots available)</h4>
           <div>
-            Sample test data obtained from API: {data.length} 
+            Sample test data obtained from API: {state.data.length} 
           </div>
-          <h4>LOWEST ({this.small_low_avail} lots available)</h4>
+          <h4>LOWEST ({state.small_low_avail} lots available)</h4>
           <div>
-            Sample data test obtained from API: {data[0].carpark_number}
+            Sample data test obtained from API: {state.data[0].carpark_number}
           </div> 
         </div>
         <div >
           <h2>MEDIUM</h2>
-          <h4>HIGHEST ({this.med_high_avail} lots available)</h4>
+          <h4>HIGHEST ({state.med_high_avail} lots available)</h4>
           <div>
-            {data[1].carpark_number}
+            {state.data[1].carpark_number}
           </div>
-          <h4>LOWEST ({this.med_low_avail} lots available)</h4>
+          <h4>LOWEST ({state.med_low_avail} lots available)</h4>
           <div>
-            {data[1].carpark_number}
+            {state.data[1].carpark_number}
           </div>
         </div>
         <div>
           <h2>BIG</h2>
-          <h4>HIGHEST ({data.length} lots available)</h4>
+          <h4>HIGHEST ({state.data.length} lots available)</h4>
           <div>
-            {data[2].carpark_number}
+            {state.data[2].carpark_number}
           </div>
-          <h4>LOWEST ({data.length} lots available)</h4>
+          <h4>LOWEST ({state.data.length} lots available)</h4>
           <div>
-            {data[2].carpark_number}
+            {state.data[2].carpark_number}
           </div>
         </div>
         <div>
           <h2>LARGE</h2>
-          <h4>HIGHEST ({data.length} lots available)</h4>
+          <h4>HIGHEST ({state.data.length} lots available)</h4>
           <div>
-            {data.length}
+            {state.data.length}
           </div>
-          <h4>LOWEST ({data.length} lots available)</h4>
+          <h4>LOWEST ({state.data.length} lots available)</h4>
           <div>
-            {data.length}
+            {state.data.length}
           </div>
         </div>
       </div>
@@ -152,7 +153,7 @@ export default class App extends Component {
   render() {
     let contents = this.state.loading
       ? <p><em>Loading...</em><br/><img src={logo} className="App-logo" alt="logo" /></p>
-      : App.renderCarpark(this.state.data);
+      : App.renderCarpark(this.state);
     
     return (
       <div className="App">
